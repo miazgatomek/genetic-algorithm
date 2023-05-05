23afed7de455d0a6dc12ad4a30ecdf7d43f94fc5
Tomasz Miazga <tom3k.miazga@gmail.com>
2023-05-05 16:50:50 +0200

    Add roulette selection

--- src/funcs.py
+++ src/funcs.py
@@ -2,6 +2,7 @@ from models import Chromosome
 from dataclasses import fields
 import numpy as np
 
+
 def eval(chrom: Chromosome):
     # return pow(chrom.x, 2) + pow(chrom.y, 2) + pow(chrom.z, 2)
     return pow(chrom.x, 2) + pow(chrom.y, 2)
@@ -12,3 +13,13 @@ def generate_chroms(num_of_chroms: int, range: list[float]):
     field_values = np.random.uniform(range[0], range[1], [num_of_chroms, chrom_class_param_count])
 
     return list(map(lambda_map_to_chroms, field_values))
+
+def get_roulette_position(rand, cum_probs: list[float]):
+    print(rand, cum_probs)
+    for i in range(len(cum_probs)):
+        if i == 0 and rand < cum_probs[0]:
+            print('zero', i, rand, cum_probs[0])
+            return 0
+        elif rand >= cum_probs[i] and rand < cum_probs[i + 1]:
+            print(rand, cum_probs[i], cum_probs[i + 1])
+            return i + 1
--- src/main.py
+++ src/main.py
@@ -1,10 +1,20 @@
 import funcs
 import constants
+import numpy as np
+
 
 chroms = funcs.generate_chroms(constants.NUM_OF_CHROMS, constants.NUM_GEN_RANGE)
+
+
 chroms_evals = list(map(lambda chrom: funcs.eval(chrom), chroms))
 chroms_fitness = list(map(lambda eval: 1 / eval, chroms_evals))
 total_fitness = sum(chroms_fitness)
 chroms_probs = list(map(lambda fitness: fitness / total_fitness, chroms_fitness))
+cum_probs = np.cumsum(chroms_probs)
 
-print(funcs.eval(chroms[1]), chroms_evals[1], total_fitness, chroms_probs[1])
+new_chroms = [None] * len(chroms)
+
+# get new generation with roulette selection
+for i in range(len(chroms)):
+    rand = np.random.uniform(0, cum_probs[-1])
+    new_chroms[i] = chroms[funcs.get_roulette_position(rand, cum_probs)]
