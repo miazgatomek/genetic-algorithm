090d097b92c8c3dcf70bfdeadb3bacecef6f0fa7
Tomasz Miazga <tom3k.miazga@gmail.com>
2023-05-05 14:40:35 +0200

    Code refactor

--- src/funcs.py
+++ src/funcs.py
@@ -2,10 +2,13 @@ from models import Chromosome
 from dataclasses import fields
 import numpy as np
 
-def fitness(c: Chromosome):
-    return pow(c.x, 2) + pow(c.y, 2)
+def fitness(chrom: Chromosome):
+    # return pow(chrom.x, 2) + pow(chrom.y, 2) + pow(chrom.z, 2)
+    return pow(chrom.x, 2) + pow(chrom.y, 2)
 
 def generate_chroms(num_of_chroms: int, range: list[float]):
     chrom_class_param_count = len(list(fields(Chromosome)))
     lambda_map_to_chroms = lambda arr: Chromosome(*arr)
-    return list(map(lambda_map_to_chroms, np.random.uniform(range[0], range[1], [num_of_chroms, chrom_class_param_count])))
+    field_values = np.random.uniform(range[0], range[1], [num_of_chroms, chrom_class_param_count])
+    
+    return list(map(lambda_map_to_chroms, field_values))
