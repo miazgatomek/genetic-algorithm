bfadf3fe2bfad6300604fb9504f2cb0355858f6f
Tomasz Miazga <tom3k.miazga@gmail.com>
2023-05-21 15:41:55 +0200

    Code beautification

--- src/constants.py
+++ src/constants.py
@@ -2,14 +2,13 @@ from dataclasses import fields
 from models import Chromosome
 from typing import Final
 
+# GA params
 NUM_GEN_RANGE: Final = [-10, 10]
 NUM_OF_CHROMS: Final = 50
 GENERATIONS: Final = 100
 MUTATION_RATE: Final = 0.3
 MUTATION_VALUE_CHANGE: Final = 0.01
 CROSSOVER_RATE: Final = 0.2
-NUM_OF_GENES: Final = len(list(fields(Chromosome)))
-CHROM_FIELDS_NAMES: Final = list(map(lambda field: field.name, fields(Chromosome)))
 
 # for tournament selection
 NUM_OF_CONTESTANTS = 10
@@ -17,3 +16,7 @@ NUM_OF_CONTESTANTS = 10
 # for evaluation
 ITERATIONS = 100
 SOLUTION = 0
+
+# generic helper constants
+NUM_OF_GENES: Final = len(list(fields(Chromosome)))
+CHROM_FIELDS_NAMES: Final = list(map(lambda field: field.name, fields(Chromosome)))
